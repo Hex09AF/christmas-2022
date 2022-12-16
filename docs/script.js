446f6eba46fbf1e53836c313eb376ba60e1f77c6
Hex09AF <lsqhuy@gmail.com>
2022-12-17 03:12:31 +0700

    More corgies and snow

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,8 @@
 var btnToggle = document.getElementById("toggle");
 var body = document.getElementById("root");
 var audio = new Audio("./sounds/Jingle-bells-short-intro.mp3");
+var corgis = document.getElementsByClassName("corgi-mode");
+
 audio.loop = true;
 var flag = 0;
 btnToggle.addEventListener("click", function() {
@@ -10,6 +12,11 @@ btnToggle.addEventListener("click", function() {
         audio.pause();
         audio.currentTime = 0;
     }
+    [...corgis].forEach(function(corgi){
+        corgi.classList.toggle("basic-corgi-mode");
+        corgi.classList.toggle("december-holiday-corgi-mode");
+    });
     flag = !flag;
     body.classList.toggle("dark");
 });
+
