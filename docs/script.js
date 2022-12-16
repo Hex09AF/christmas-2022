a12a3ebe345db2da9cc0c9694d93cb65689066c8
Hex09AF <lsqhuy@gmail.com>
2022-12-17 02:05:20 +0700

    Add jingle bell sound

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,15 @@
 var btnToggle = document.getElementById("toggle");
 var body = document.getElementById("root");
+var audio = new Audio("./sounds/Jingle-bells-short-intro.mp3");
+audio.loop = true;
+var flag = 0;
 btnToggle.addEventListener("click", function() {
-    console.log("HIHI");
+    if (!flag) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+    flag = !flag;
     body.classList.toggle("dark");
 });
